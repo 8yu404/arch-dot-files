a3eba0f5024e0eeee2d4548b7a83cfbcb9004372
Sara Yuzuki <sara.yuzuki@proton.me>
2025-03-11 15:27:26 +0900

    added forwarding rules and scripts for waydroid

--- waydroid/set.fish
+++ waydroid/set.fish
@@ -5,3 +5,8 @@ repo sync build/make
 wget -O - https://raw.githubusercontent.com/waydroid/android_vendor_waydroid/lineage-20/manifest_scripts/generate-manifest.sh | bash
 nvim .repo/manifests/default.xml
 repo sync
+
+firewall-cmd --zone=trusted --add-port=67/udp
+firewall-cmd --zone=trusted --add-port=53/udp
+firewall-cmd --zone=trusted --add-forward
+firewall-cmd --zone=trusted --add-interface=waydroid0
